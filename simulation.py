554795793ae52d8c25b21eeb95631433ac4d7182
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2018-11-20 01:37:49 -0700

    6 implemented
    answered q 6, routes on larger topology with a seperate path for 1->3 and 3->1

--- simulation.py
+++ simulation.py
@@ -6,7 +6,7 @@ import sys
 
 ##configuration parameters
 router_queue_size = 0 #0 means unlimited
-simulation_time = 10   #give the network sufficient time to execute transfers
+simulation_time = 5   #give the network sufficient time to execute transfers
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads at the end
@@ -78,9 +78,9 @@ if __name__ == '__main__':
             obj.print_routes()
 
     #send packet from host 1 to host 2
-    # host_1.udt_send('H3', 'MESSAGE_FROM_H1')
-    # sleep(simulation_time)
-    # host_3.udt_send('H1', 'MESSAGE_FROM_H3')
+    host_1.udt_send('H3', 'MESSAGE_FROM_H1')
+    sleep(simulation_time)
+    host_3.udt_send('H1', 'MESSAGE_FROM_H3')
     sleep(simulation_time)
     
     
