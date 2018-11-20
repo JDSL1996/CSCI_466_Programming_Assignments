745f3db3c127f2a197c77b7f79e2efd4e56a52c5
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2018-11-20 00:51:49 -0700

    create topology
    currently the update is not functioning for this topology but the structure is now implemented

--- simulation.py
+++ simulation.py
@@ -6,7 +6,7 @@ import sys
 
 ##configuration parameters
 router_queue_size = 0 #0 means unlimited
-simulation_time = 1   #give the network sufficient time to execute transfers
+simulation_time = 10   #give the network sufficient time to execute transfers
 
 if __name__ == '__main__':
     object_L = [] #keeps track of objects, so we can kill their threads at the end
@@ -16,19 +16,33 @@ if __name__ == '__main__':
     object_L.append(host_1)
     host_2 = network.Host('H2')
     object_L.append(host_2)
+    host_3 = network.Host('H3')
+    object_L.append(host_3)
     
     #create routers and cost tables for reaching neighbors
-    cost_D = {'H1': {0: 1}, 'RB': {1: 1}} # {neighbor: {interface: cost}}
+    cost_D = {'H1': {0: 1}, 'RB': {1: 1}, 'RC': {2: 3}, "H2": {3: 1}} # {neighbor: {interface: cost}}
     router_a = network.Router(name='RA', 
                               cost_D = cost_D,
                               max_queue_size=router_queue_size)
     object_L.append(router_a)
 
-    cost_D = {'H2': {1: 3}, 'RA': {0: 1}} # {neighbor: {interface: cost}}
+    cost_D = {'RA': {0: 1}, 'RD': {1: 3}} # {neighbor: {interface: cost}}
     router_b = network.Router(name='RB', 
                               cost_D = cost_D,
                               max_queue_size=router_queue_size)
     object_L.append(router_b)
+
+    cost_D = {'RA': {0: 3}, 'RD': {1: 1}}  # {neighbor: {interface: cost}}
+    router_c = network.Router(name='RC',
+                              cost_D=cost_D,
+                              max_queue_size=router_queue_size)
+    object_L.append(router_c)
+
+    cost_D = {'RB': {0: 3}, 'H3': {1: 1}, 'RC': {2: 1}}  # {neighbor: {interface: cost}}
+    router_d = network.Router(name='RD',
+                              cost_D=cost_D,
+                              max_queue_size=router_queue_size)
+    object_L.append(router_d)
     
     #create a Link Layer to keep track of links between network nodes
     link_layer = link.LinkLayer()
@@ -37,7 +51,14 @@ if __name__ == '__main__':
     #add all the links - need to reflect the connectivity in cost_D tables above
     link_layer.add_link(link.Link(host_1, 0, router_a, 0))
     link_layer.add_link(link.Link(router_a, 1, router_b, 0))
-    link_layer.add_link(link.Link(router_b, 1, host_2, 0))
+    link_layer.add_link(link.Link(router_a, 2, router_c, 0))
+    link_layer.add_link(link.Link(router_a, 3, host_2, 0))
+
+    link_layer.add_link(link.Link(router_b, 1, router_d, 0))
+
+    link_layer.add_link(link.Link(router_c, 1, router_d, 2))
+
+    link_layer.add_link(link.Link(router_d, 1, host_3, 0))
     
     
     #start all the objects
@@ -57,9 +78,9 @@ if __name__ == '__main__':
             obj.print_routes()
 
     #send packet from host 1 to host 2
-    host_1.udt_send('H2', 'MESSAGE_FROM_H1')
-    sleep(simulation_time)
-    host_2.udt_send('H1', 'MESSAGE_FROM_H2')
+    # host_1.udt_send('H3', 'MESSAGE_FROM_H1')
+    # sleep(simulation_time)
+    # host_3.udt_send('H1', 'MESSAGE_FROM_H3')
     sleep(simulation_time)
     
     
